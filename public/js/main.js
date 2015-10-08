c4cd5f5d70fdccbb58c4a9c87b376f61cf92bd82
Jeff Skelton <jeff@jefferyskelton.com>
2015-10-07 21:19:47 -0700

    set up contact form ajax

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,20 @@ $(function(){
         pagination: '.swiper-pagination',
         paginationClickable: true
     });
+
+    $('#mobileContactForm').find('form').on('submit', onMobileFormSubmit);
+
+    function onMobileFormSubmit(e){
+        e.preventDefault();
+        $.post('/contact', {
+            email : $('#mobileContactFormEmail').val(),
+            name : $('#mobileContactFormName').val(),
+            message : $("#mobileContactFormMessage").val(),
+            number : $("#mobileContactFormMessage").val()
+        }).success(function(){
+            alert('success!!!')
+        }).error(function(){
+            alert('error');
+        });
+    }
 });
