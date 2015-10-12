7c2198cbe96d41d90e13b97f7ec93a596a70b9c7
Jeff Skelton <jeff@jefferyskelton.com>
2015-10-12 16:18:19 -0700

    starting desktop handling

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,60 @@
 $(function(){
-    var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
 
-    $('#mobileContactForm').find('form').on('submit', onMobileFormSubmit);
-
-    function onMobileFormSubmit(e){
-        e.preventDefault();
-        $.post('/contact', {
-            email : $('#mobileContactFormEmail').val(),
-            name : $('#mobileContactFormName').val(),
-            message : $("#mobileContactFormMessage").val(),
-            number : $("#mobileContactFormMessage").val()
-        }).success(function(){
-            alert('success!!!')
-        }).error(function(){
-            alert('error');
-        });
+    if(isMobile()){
+        initMobileUI();
+    }else{
+        initDesktopUI();
     }
 
-    function onDesktopFormSubmit(e){
-        e.preventDefault();
-        $.post('/contact', {
-            email : $('#mobileContactFormEmail').val(),
-            name : $('#mobileContactFormName').val(),
-            message : $("#mobileContactFormMessage").val(),
-            number : $("#mobileContactFormMessage").val()
-        }).success(function(){
-            alert('success!!!')
-        }).error(function(){
-            alert('error');
+    function initMobileUI(){
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true
         });
+        $('body').addClass('mobile');
+        $('#mobileContactForm').find('form').on('submit', onMobileFormSubmit);
+
+        function onMobileFormSubmit(e){
+            e.preventDefault();
+            $.post('/contact', {
+                email : $('#mobileContactFormEmail').val(),
+                name : $('#mobileContactFormName').val(),
+                message : $("#mobileContactFormMessage").val(),
+                number : $("#mobileContactFormMessage").val()
+            }).success(function(){
+                alert('success!!!')
+            }).error(function(){
+                alert('error');
+            });
+        }
+    }
+
+
+    function initDesktopUI(){
+
+        var timer = setTimeout(function(){
+            $('#logo_slide').fadeOut('slow');
+        }, 1000);
+
+        function onDesktopFormSubmit(e){
+            e.preventDefault();
+            $.post('/contact', {
+                email : $('#mobileContactFormEmail').val(),
+                name : $('#mobileContactFormName').val(),
+                message : $("#mobileContactFormMessage").val(),
+                number : $("#mobileContactFormMessage").val()
+            }).success(function(){
+                alert('success!!!')
+            }).error(function(){
+                alert('error');
+            });
+        }
+    }
+
+
+    function isMobile(){
+        var pattern = /(Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune)/i,
+          ua = navigator.userAgent;
+        return pattern.test(ua);
     }
 });
